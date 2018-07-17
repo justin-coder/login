f945babea0f9e911ab7c6b69416b72fe80371ab9
justin-coder <41302805+justin-coder@users.noreply.github.com>
2018-07-17 18:27:18 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$db_name =""; //Database Name.
+$db_user =""; //Database Username.
+$db_password =""; //Database Password.
+$db_server =""; //Database Server Name or Host Name . EX. localhost
+?>
